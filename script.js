08143c01f6da7b69e3ea620b3270430adf32590a
RylanSM <166328097+RylanSM@users.noreply.github.com>
2024-05-21 13:43:36 -0500

    Turning in secure the vault workshop

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const one = (7 + 3);
+
+//Calculates first combo
+
+const two = (80 / 2);
+
+//Calculates Second Combo
+
+const three =(3 * 13);
+
+//Calculates Third Combo
+
+//Alert Message to User
+alert("You have receieved this message because you have been chosen to open an important vault. Here is the secret combination:" + " " + one + " " + two + " " + three); 
+
+
+
+
